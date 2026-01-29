c571d0693939bf7fc6d08d2120cf03c06734cd40
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-01-29 15:26:32 +0530

    Update preview.js

--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,4 @@
-import * as Tests from "../tests/index.js";
+import * as Tests from "./tests/index.js";
 
 const patient = JSON.parse(localStorage.getItem("patient"));
 const report = JSON.parse(localStorage.getItem("report"));
@@ -131,3 +131,4 @@ window.download = function () {
     })
     .save();
 };
+
