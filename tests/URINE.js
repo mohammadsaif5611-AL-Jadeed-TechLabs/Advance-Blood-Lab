2a3a0e0de09e9df177ecd848a3243f48139dffee
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-01-29 15:33:36 +0530

    Add files via upload

---
+++ tests/URINE.js
@@ -0,0 +1,38 @@
+export default {
+  title: "URINE ANALYSIS REPORT",
+
+  sections: [
+    {
+      name: "PHYSICAL EXAMINATION",
+      fields: [
+        ["QUANTITY", "ML"],
+        ["COLOUR", ""],
+        ["NATURE", ""],
+        ["APPEARANCE", ""],
+        ["REACTION", ""]
+      ]
+    },
+    {
+      name: "CHEMICAL EXAMINATION",
+      fields: [
+        ["ALBUMIN", ""],
+        ["SUGAR", ""],
+        ["BILE SALT", ""],
+        ["BILE PIGMENT", ""]
+      ]
+    },
+    {
+      name: "MICROSCOPIC EXAMINATION",
+      fields: [
+        ["R.B.C.", ""],
+        ["PUS CELLS", ""],
+        ["EPITHELIAL CELLS", ""],
+        ["AMORPHOUS MATERIAL", ""],
+        ["BACTERIA", ""],
+        ["CAST", ""],
+        ["CRYSTALS", ""],
+        ["OTHER FINDINGS", ""]
+      ]
+    }
+  ]
+};
