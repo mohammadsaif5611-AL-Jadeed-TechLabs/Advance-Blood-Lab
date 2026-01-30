10cdb2770057436f010ca1ebcd703427090f96eb
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-01-30 17:11:36 +0530

    Add files via upload

---
+++ tests/BLOOD_SUGAR_TEST.js
@@ -0,0 +1,30 @@
+export default {
+  title: "BIOCHEMISTRY REPORT",
+  subtitle: "BLOOD SUGAR TEST",
+  fields: [
+    {
+      name: "BLOOD SUGAR FASTING",
+      unit: "mg/dl",
+      ref: "70 - 110",
+      type: "decimal"
+    },
+    {
+      name: "BLOOD SUGAR POSTMEAL",
+      sub: "(AFTER 1 & 1/2 HOURS)",
+      unit: "mg/dl",
+      ref: "70 - 160",
+      type: "decimal"
+    }
+  ]
+};
+
+
+// BLOOD SUGAR TEST
+
+// 177
+// H
+
+
+// 221
+// H
+
