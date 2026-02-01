1492c30ce55d4335e9ea77472bf3173b2316e8ce
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-01 22:57:43 +0530

    Add files via upload

---
+++ tests/LIVER_FUNCTION.js
@@ -0,0 +1,22 @@
+export default {
+  key: "LIVER_FUNCTION_TEST",
+  title: "BIOCHEMISTRY REPORT",
+  subtitle: "LIVER FUNCTION TEST",
+  class: "LIVER FUNCTION TEST",
+  testname: "LIVER FUNCTION TEST",
+
+  fields: [
+    { name: "SERUM BILIRUBIN TOTAL", unit: "mg/dl", ref: "0.2 - 1.2" },
+    { name: "SERUM BILIRUBIN DIRECT", unit: "mg/dl", ref: "0.1 - 0.4" },
+    { name: "SERUM BILIRUBIN INDIRECT", unit: "mg/dl", ref: "0.2 - 1.0" },
+
+    { name: "SERUM S G O T", unit: "U/L", ref: "10 - 40" },
+    { name: "SERUM S G P T", unit: "U/L", ref: "10 - 40" },
+    { name: "SR. ALK PHOSPHATASE", unit: "U/L", ref: "40 - 129" },
+
+    { name: "SR. PROTEIN", unit: "gm/dl", ref: "6.6 - 8.7" },
+    { name: "SR. ALBUMIN", unit: "gm/dl", ref: "3.5 - 5.2" },
+    { name: "SR. GLOBULIN", unit: "gm/dl", ref: "2.8 - 3.6" },
+    { name: "A/G RATIO", unit: "", ref: "0.9 - 2" }
+  ]
+};
