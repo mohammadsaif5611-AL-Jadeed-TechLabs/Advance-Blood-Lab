f706ca29d80c574d6cbb51e8a9de1ec7c9362a2d
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-01 21:02:21 +0530

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,15 @@
 import CBC from "./CBC.js";
+import SERUM_SGOT from "./SERUM_SGOT.js";
+import SERUM_SGPT from "./SERUM_SGPT.js";
+import SERUM_BILIRUBIN from "./SERUM_BILIRUBIN.js";
 import URINE from "./URINE.js";
 import BLOOD_SUGAR from "./BLOOD_SUGAR_TEST.js";
 
 export default {
   CBC,
   URINE,
-  BLOOD_SUGAR
+  BLOOD_SUGAR,
+  SERUM_SGOT,
+  SERUM_SGPT,
+  SERUM_BILIRUBIN,
 };
