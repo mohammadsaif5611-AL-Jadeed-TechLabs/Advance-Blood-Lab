e17f9288e84c60a4f750245d76aebc775605ab27
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-01 22:56:09 +0530

    Delete tests/SERUM_BILIRUBIN.js

--- tests/SERUM_BILIRUBIN.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title: "BIOCHEMISTRY REPORT",
-  testname: "SERUM BILIRUBIN",
-  fields: [
-    ["SERUM BILIRUBIN TOTAL", "mg/dl", "0.2 - 1.2"],
-    ["DIRECT", "mg/dl", "0.0 - 0.2"],
-    ["INDIRECT", "mg/dl", "0.2 - 1.0"],
-  ]
-};
