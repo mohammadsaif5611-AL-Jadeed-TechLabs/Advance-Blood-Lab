1edc774b2d81519f7ba251cb122876ea1c2e57be
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-04 11:36:25 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// import * as Tests from "../tests/index.js";
+import Tests from "../tests/index.js";
+
+console.log("Tests object:", Tests);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("main.js loaded", Tests);
+
+  const testBox = document.getElementById("tests");
+  const form = document.getElementById("patientForm");
+
+  if (!testBox || !form) {
+    alert("HTML IDs missing");
+    return;
+  }
+
+  Object.keys(Tests).forEach(key => {
+    testBox.innerHTML += `
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="${key}">
+        <label class="form-check-label">
+          ${Tests[key].testname}
+        </label>
+      </div>`;
+  });
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const patient = {
+      name: document.getElementById("patient_name").value,
+      age: document.getElementById("age").value,
+      gender: document.getElementById("sex").value,
+      doctor: document.getElementById("ref_by").value,
+      sample: document.getElementById("sample").value,
+      date: document.getElementById("date").value,
+      lrn: document.getElementById("lrn").value
+    };
+
+    localStorage.setItem("patient", JSON.stringify(patient));
+
+    const selected = [...document.querySelectorAll("#tests input:checked")]
+      .map(i => i.value);
+
+    if (selected.length === 0) {
+      alert("Please select at least one test");
+      return;
+    }
+
+    localStorage.setItem("tests", JSON.stringify(selected));
+    window.location.href = "test-input.html";
+  });
+
+
+
+});
