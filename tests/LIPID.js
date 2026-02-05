df6ff0691b6edde2b00b8b1e69c92c11d07ac44e
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-05 20:08:24 +0530

    Add files via upload

---
+++ tests/LIPID.js
@@ -0,0 +1,45 @@
+export default {
+  key: "LIPID",
+  title: "BIOCHEMISTRY REPORT",
+  subtitle: "LIPID PROFILE",
+  class: "LIPID REPORT",
+  testname: "LIPID PROFILE",
+
+  fields: [
+    {
+      name: "TOTAL CHOLESTEROL",
+      unit: "mg/dl",
+      ref: "150 - 250"
+    },
+    {
+      name: "TRIGLYCERIDES",
+      unit: "mg/dl",
+      ref: "75 - 110"
+    },
+    {
+      name: "HDL CHOLESTEROL",
+      unit: "mg/dl",
+      ref: "30 - 70"
+    },
+    {
+      name: "LDL CHOLESTEROL",
+      unit: "mg/dl",
+      ref: "Upto - 135"
+    },
+    {
+      name: "VLDL CHOLESTEROL",
+      unit: "mg/dl",
+      ref: "Upto - 35"
+    },
+    {
+      name: "T CHOLE. / HDL CHOLE. RATIO",
+      unit: "",
+      ref: "Upto - 5.0"
+    },
+    {
+      name: "LDL / HDL RATIO",
+      unit: "",
+      ref: "M - < 3.55, F - < 3.22"
+    }
+  ]
+};
