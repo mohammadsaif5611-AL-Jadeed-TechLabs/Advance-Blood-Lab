62c168d6ad97017d44b4dd6877aeb7d80b5259bb
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-05 16:12:53 +0530

    Update LIVER_FUNCTION.js

--- tests/LIVER_FUNCTION.js
+++ tests/LIVER_FUNCTION.js
@@ -5,10 +5,10 @@ export default {
   class: "LIVER FUNCTION TEST",
   testname: "LIVER FUNCTION TEST",
 
-  fields: [
+  fields: [  
     { name: "SERUM BILIRUBIN TOTAL", unit: "mg/dl", ref: "0.2 - 1.2" },
-    { name: "SERUM BILIRUBIN DIRECT", unit: "mg/dl", ref: "0.1 - 0.4" },
-    { name: "SERUM BILIRUBIN INDIRECT", unit: "mg/dl", ref: "0.2 - 1.0" },
+    { name: "DIRECT", unit: "mg/dl", ref: "0.0 - 0.2" },
+    { name: "INDIRECT", unit: "mg/dl", ref: "0.2 - 1.0" },
 
     { name: "SERUM S G O T", unit: "U/L", ref: "10 - 40" },
     { name: "SERUM S G P T", unit: "U/L", ref: "10 - 40" },
@@ -16,7 +16,7 @@ export default {
 
     { name: "SR. PROTEIN", unit: "gm/dl", ref: "6.6 - 8.7" },
     { name: "SR. ALBUMIN", unit: "gm/dl", ref: "3.5 - 5.2" },
-    { name: "SR. GLOBULIN", unit: "gm/dl", ref: "2.8 - 3.6" },
+    { name: "SR. GLOBULIN", unit: "gm/dl", ref: "2.8 - 3.6" },  
     { name: "A/G RATIO", unit: "", ref: "0.9 - 2" }
   ]
 };
