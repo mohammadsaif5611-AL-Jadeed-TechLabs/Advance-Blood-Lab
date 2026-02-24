246a952199616ec1b03e459a46a1b767c48909a1
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-24 16:56:49 +0530

    Update COAGUL.js

--- tests/COAGUL.js
+++ tests/COAGUL.js
@@ -6,20 +6,27 @@ export default {
   testname: "COAGULATION",
 
   fields: [
-    { name: "Bleeding Time", unit: "m/s", ref: "2 - 5 min." },
-    { name: "Clotting Time", unit: "m/s", ref: "3 - 7 min." },
 
-    // ✅ ELECTROLYTES SUB HEADING
-    {
-      sub: "PROTHROMBIN TIME (PT)"
-    },
+    { name: "Bleeding Time", unit: "min", ref: "2 - 5" },
+    { name: "Clotting Time", unit: "min", ref: "3 - 7" },
+
+    { sub: "ACTIVATED PARTIAL THROMBOPLASTIN TIME (APTT)" },
+
+    { name: "aPTT Patient Value", unit: "seconds", ref: "22 - 35" },
+    { name: "aPTT Control Value", unit: "seconds", ref: "" },
+    { name: "aPTT Ratio", unit: "", ref: "" },
+
+    { sub: "PROTHROMBIN TIME (PT)" },
 
     { name: "Patient Value (PT)", unit: "seconds", ref: "11 - 16" },
     { name: "Control Value (CT)", unit: "seconds", ref: "" },
-    { name: "Prothrombin Index", unit: "%", ref: ""  },
+    { name: "Prothrombin Index", unit: "%", ref: "" },
     { name: "Prothrombin Ratio", unit: "", ref: "" },
-    { name: "I.S.I. Value",  unit: "", ref: ""  },
-    { name: "International Normalised Ratio (INR)",  unit: "", ref: "" },
+    { name: "I.S.I. Value", unit: "", ref: "1.0 - 1.4" },
+    { name: "International Normalised Ratio (INR)", unit: "", ref: "" },
+
+    { sub: "PLATELET" },
+
+    { name: "Platelet Count", unit: "/cumm", ref: "1,50,000 - 4,50,000" }
   ]
 };
-
