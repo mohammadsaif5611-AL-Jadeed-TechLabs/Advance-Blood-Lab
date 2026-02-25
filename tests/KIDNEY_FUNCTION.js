e55eaa1e59e8e3eb396e38842af6b84d93a993cb
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-25 14:57:14 +0530

    Update KIDNEY_FUNCTION.js

--- tests/KIDNEY_FUNCTION.js
+++ tests/KIDNEY_FUNCTION.js
@@ -1,24 +1,27 @@
 export default {
-  key: "KIDNEY_FUNCTION_TEST",
+  key: "LIVER_FUNCTION_TEST",
   title: "BIOCHEMISTRY REPORT",
-  subtitle: "KIDNEY FUNCTION TEST",
-  class: "KIDNEY FUNCTION TEST",
-  testname: "KIDNEY FUNCTION TEST",
+  subtitle: "LIVER FUNCTION TEST",
+  class: "LIVER FUNCTION TEST",
+  testname: "LIVER FUNCTION TEST",
 
   fields: [
-    { name: "SR. BLOOD UREA", unit: "mg/dl", ref: "10 - 50" },
-    { name: "BLOOD UREA NITROGEN", unit: "mg/dl", ref: "7 - 18" },
-    { name: "SR. CREATININE", unit: "mg/dl", ref: "0.6 - 1.2" },
-    { name: "SR. URIC ACID", unit: "mg/dl", ref: "2.5 - 6.8" },
 
-    // ✅ ELECTROLYTES SUB HEADING
-    {
-      sub: "ELECTROLYTES"
-    },
+    { name: "SERUM BILIRUBIN TOTAL", unit: "mg/dl", ref: "0.2 - 1.2" },
+    { name: "DIRECT", unit: "mg/dl", ref: "0.0 - 0.2" },
+    { name: "INDIRECT", unit: "mg/dl", ref: "0.2 - 1.0" },
 
-    { name: "SR. SODIUM", unit: "mmol/L", ref: "135 - 155" },
-    { name: "SR. POTASSIUM", unit: "mmol/L", ref: "3.5 - 5.3" },
-    { name: "SR. IONIC CALCIUM", unit: "mmol/L", ref: "1.1 - 1.3" },
-    { name: "SR. CHLORIDE", unit: "mmol/L", ref: "98 - 106" }
+    { name: "SERUM S G O T", unit: "U/L", ref: "10 - 40" },
+    { name: "SERUM S G P T", unit: "U/L", ref: "10 - 40" },
+
+    { name: "SERUM ALKALINE PHOSPHATASE", unit: "IU/L", ref: "37 - 147 | Paediatric : 210 - 515" },
+
+    { name: "SERUM PROTEINS TOTAL", unit: "gm/dl", ref: "6.0 - 8.0" },
+    { name: "ALBUMIN", unit: "gm/dl", ref: "3.2 - 5.0" },
+    { name: "GLOBULIN", unit: "gm/dl", ref: "2.0 - 4.0" },
+
+    { name: "AG RATIO", unit: "", ref: "1.1 - 2.5" },
+
+    { name: "GAMMA GT", unit: "U/L", ref: "5 - 45" }
   ]
 };
