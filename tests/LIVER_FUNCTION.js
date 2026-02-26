36fe2df69ef6c2a3f7d7877ab469d16ae96a9a0d
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-02-26 12:00:57 +0530

    Refactor liver function test fields and references
    Updated field names and reference ranges for liver function tests.

--- tests/LIVER_FUNCTION.js
+++ tests/LIVER_FUNCTION.js
@@ -5,18 +5,23 @@ export default {
   class: "LIVER FUNCTION TEST",
   testname: "LIVER FUNCTION TEST",
 
-  fields: [  
+  fields: [
+
     { name: "SERUM BILIRUBIN TOTAL", unit: "mg/dl", ref: "0.2 - 1.2" },
     { name: "DIRECT", unit: "mg/dl", ref: "0.0 - 0.2" },
     { name: "INDIRECT", unit: "mg/dl", ref: "0.2 - 1.0" },
 
     { name: "SERUM S G O T", unit: "U/L", ref: "10 - 40" },
     { name: "SERUM S G P T", unit: "U/L", ref: "10 - 40" },
-    { name: "SR. ALK PHOSPHATASE", unit: "U/L", ref: "40 - 129" },
 
-    { name: "SR. PROTEIN", unit: "gm/dl", ref: "6.6 - 8.7" },
-    { name: "SR. ALBUMIN", unit: "gm/dl", ref: "3.5 - 5.2" },
-    { name: "SR. GLOBULIN", unit: "gm/dl", ref: "2.8 - 3.6" },  
-    { name: "A/G RATIO", unit: "", ref: "0.9 - 2" }
+    { name: "SERUM ALKALINE PHOSPHATASE", unit: "IU/L", ref: "37 - 147 | Paediatric : 210 - 515" },
+
+    { name: "SERUM PROTEINS TOTAL", unit: "gm/dl", ref: "6.0 - 8.0" },
+    { name: "ALBUMIN", unit: "gm/dl", ref: "3.2 - 5.0" },
+    { name: "GLOBULIN", unit: "gm/dl", ref: "2.0 - 4.0" },
+
+    { name: "AG RATIO", unit: "", ref: "1.1 - 2.5" },
+
+    { name: "GAMMA GT", unit: "U/L", ref: "5 - 45" }
   ]
 };
